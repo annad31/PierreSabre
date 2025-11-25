96a1e00b0b5d6654cdd679b83d7b82dfd1118cfd
annad31 <anna.denis@utoulouse.fr>
2025-11-25 19:47:59 +0100

    TP5 partie 1

--- src/personnages/Humain.java
+++ src/personnages/Humain.java
@@ -4,6 +4,8 @@ public class Humain {
 	private String nom;
 	private String boisson;
 	private int argent;
+	protected int nbConnaissances;
+	protected Humain[] memoire = new Humain[30];
 	
 	public Humain(String nom, String boisson, int argent) {
 		super();
@@ -46,5 +48,42 @@ public class Humain {
 	
 	protected void gagnerArgent(int gain) {
 		this.argent=this.argent+gain;
+	}
+	
+	public void faireConnaissanceAvec(Humain humain) {
+		direBonjour();
+		humain.repondre(this);
+		memoriser(humain);
+	}
+	
+	private void repondre(Humain humain) {
+		direBonjour();
+		memoriser(humain);
+	}
+	
+	private void memoriser(Humain humain) {
+		if (this.nbConnaissances==30) {
+			for(int i=1;i<this.nbConnaissances;i++) {
+				memoire[i-1]=memoire[i];
+			}
+			this.memoire[nbConnaissances-1]=humain;
+		}
+		
+		else {
+			this.memoire[nbConnaissances]=humain;
+			this.nbConnaissances+=1;
+		}
+		
+	}
+	
+	public void listerConnaissance() {
+		this.parler("Je connais beaucoup de monde dont : ");
+		for (int i=0;i<this.nbConnaissances;i++) {
+			System.out.print(memoire[i].getNom());
+			if(i+1<this.nbConnaissances) {
+				System.out.print(", ");
+			}
+		}
+		System.out.println("");
 	}
 }
