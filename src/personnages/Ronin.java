b1fd2a87ef69c9bb211e7dd906283eff44e8e298
annad31 <anna.denis@utoulouse.fr>
2025-11-25 19:00:09 +0100

    TP4 fini

--- src/personnages/Ronin.java
+++ src/personnages/Ronin.java
@@ -13,4 +13,21 @@ public class Ronin extends Humain {
 		this.perdreArgent(don);
 		beneficiaire.recevoir(don);
 	}
+	
+	public void provoquer(Yakuza adversaire) {
+		parler("Je t'ai retrouvé vermine, tu vas payer pour ce que tu as fait à ce pauvre marchand !");
+		int force=4*this.honneur;
+		if (force>=adversaire.getReputation()) {
+			parler("Je t'ai eu petit yakuza !");
+			int gain=adversaire.perdre();
+			this.gagnerArgent(gain);
+			this.honneur=honneur+1;
+		}
+		else {
+			int perte=this.getArgent();
+			this.perdreArgent(perte);
+			adversaire.gagner(perte);
+			parler("J'ai perdu contre ce yakuza, mon honneur et ma bourse en ont pris un coup");
+		}
+	}
 }
--- src/personnages/Yakuza.java
+++ src/personnages/Yakuza.java
@@ -3,13 +3,17 @@ import personnages.Commercant;
 
 public class Yakuza extends Humain {
 	private String clan;
-	private int reputation=0;
+	private int reputation=4;
 	
 	public Yakuza(String nom, String boisson, int argent, String clan) {
 		super(nom,boisson,argent);
 		this.clan = clan;
 	}
 	
+	public int getReputation() {
+		return reputation;
+	}
+
 	public void extorquer(Commercant victime) {
 		String nomVictime=victime.getNom();
 		parler(nomVictime + " si tu tiens à la vie, donne moi ta bourse !");
@@ -17,5 +21,20 @@ public class Yakuza extends Humain {
 		this.gagnerArgent(gain);
 		parler("J'ai piqué les " + gain + " sous de " + nomVictime + ", ce qui me fait " + this.getArgent() + " sous dans ma poche. Hi ! Hi !");
 		this.reputation=this.reputation+1;
+	}
+	
+	public int perdre() {
+		int perte=this.getArgent();
+		this.perdreArgent(perte);
+		this.reputation=this.reputation-1;
+		parler("J'ai perdu le duel et mes " + perte + " sous, snif ... J'ai déshonoré le clan de "+ this.clan + ".");
+		return perte;
+	}
+	
+	public int gagner(int gain) {
+		this.gagnerArgent(gain);
+		this.reputation=this.reputation+1;
+		parler("Ce ronin pensait vraiment battre " + this.getNom() + " du clan de " + this.clan + ". Je l'ai dépouillé de ses " + gain + " sous.");
+		return this.getArgent();
 	}
 }
